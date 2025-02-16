9af1b0c752ed11e6e3f3a1c60498f61f71de838b
Jo Vliegen <jo.vliegen@kuleuven.be>
2025-02-16 08:13:09 +0100

    with prev

--- static/src/20/LED_parasite.vhd
+++ static/src/20/LED_parasite.vhd
@@ -4,7 +4,7 @@ begin
         if reset = '1' then 
             leds <= "0000";
         else
-            if dmem_we = '1' and dmem_a = x"88000000" then 
+            if dmem_we = '1' and dmem_a = x"80000000" then 
                 leds <= dmem_di(3 downto 0);
             end if;
         end if;
